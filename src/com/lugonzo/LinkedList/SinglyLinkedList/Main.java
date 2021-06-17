5a5b45c5ce1847ae820ae2019237977f863862b0
Abuu001 <abulugz@gmail.com>
2021-06-17 16:58:36 +0300

    SLL main method

--- src/com/lugonzo/LinkedList/SinglyLinkedList/Main.java
+++ src/com/lugonzo/LinkedList/SinglyLinkedList/Main.java
@@ -1,2 +1,22 @@
-package com.lugonzo.LinkedList.SinglyLinkedList;public class Main {
+package com.lugonzo.LinkedList.SinglyLinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+
+        intLinkedList list = new intLinkedList(3);
+        list.insertItemAtStart(5);
+        list.insertItemAtStart(8);
+        list.insertItemAtStart(9);
+        list.insertItemAtStart(11);
+
+        list.printList(); // 11, 9,8,5,3
+        System.out.println("===================================");
+        list.deleteNode(5);
+        list.printList();  // 11, 9,8,3
+
+        System.out.println("===================================");
+        list.insertItemAtEnd(7);
+        list.printList();
+    }
 }
