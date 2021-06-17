3b6410b6f7633c371ea37fee76e8eb2e3ac02710
Abuu001 <abulugz@gmail.com>
2021-06-17 17:00:00 +0300

    Queue main method

--- src/com/lugonzo/Queue/Main.java
+++ src/com/lugonzo/Queue/Main.java
@@ -1,2 +1,21 @@
-package com.lugonzo.Queue;public class Main {
+package com.lugonzo.Queue;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+
+        Intq q = new Intq();
+        q.enqueue(3);
+        q.enqueue(4);
+        q.enqueue(5);
+        q.enqueue(6);
+        q.enqueue(7);
+
+        //first in first out
+        System.out.println(q.dequeue() ); //3
+        System.out.println(q.dequeue() ); //4
+        System.out.println(q.dequeue() );//5
+        System.out.println(q.dequeue() );//6
+    }
 }
