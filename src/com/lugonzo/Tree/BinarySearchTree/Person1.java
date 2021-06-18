2ec92c5854fe4a70b4efa451be1568f5dd647c23
Abuu001 <abulugz@gmail.com>
2021-06-18 21:07:29 +0300

    person properties

--- src/com/lugonzo/Tree/BinarySearchTree/Person1.java
+++ src/com/lugonzo/Tree/BinarySearchTree/Person1.java
@@ -1,2 +1,27 @@
-package com.lugonzo.Tree.BinarySearchTree;public class Person1 {
+package com.lugonzo.Tree.BinarySearchTree;
+
+public class Person1 {
+    private String name;
+    private int age;
+
+    public Person1(String n , int a){
+        this.name=n;
+        this.age=a;
+    }
+
+    public String toString(){
+        return "[{Name " + this.name + " , age " + this.age +"} ]";
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public void setAge(int a) {
+        this.age = a;
+    }
 }
