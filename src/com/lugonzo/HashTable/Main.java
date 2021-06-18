59c35ad9c119d42bd53da9c0283f6627ff6de028
Abuu001 <abulugz@gmail.com>
2021-06-18 21:05:53 +0300

    Hash tble main method

--- src/com/lugonzo/HashTable/Main.java
+++ src/com/lugonzo/HashTable/Main.java
@@ -1,2 +1,8 @@
-package com.lugonzo.HashTable;public class Main {
+package com.lugonzo.HashTable;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+    }
 }
