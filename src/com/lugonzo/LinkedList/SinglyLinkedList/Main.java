f193c3a1cddca93f31eff59ead94c5a79f5f83c1
Abuu001 <abulugz@gmail.com>
2021-06-23 00:16:22 +0300

    Md

--- src/com/lugonzo/LinkedList/SinglyLinkedList/Main.java
+++ src/com/lugonzo/LinkedList/SinglyLinkedList/Main.java
@@ -18,5 +18,17 @@ public class Main {
         System.out.println("===================================");
         list.insertItemAtEnd(7);
         list.printList();
+
+        System.out.println("=============Linked List 2 (sLinkedList)======================");
+
+        sLinkedList list2 = new sLinkedList();
+        list2.insertFirst(22);
+        list2.insertFirst(23);
+        list2.insertFirst(24);
+        list2.insertFirst(25); // 25 => 24 => 23 => 22 =>
+
+        list2.addNodeAtLast(33);  //25 => 24 => 23 => 22 => 33 =>
+        list2.printAll();
+
     }
 }
