8d0bf58e0ce4d3ad47cd24d33a077168045aa41b
Abuu001 <abulugz@gmail.com>
2021-06-23 22:59:39 +0300

    impl2

--- src/com/lugonzo/Tree/BinarySearchTree/sBinarySearchTree.java
+++ src/com/lugonzo/Tree/BinarySearchTree/sBinarySearchTree.java
@@ -1,2 +1,24 @@
-package com.lugonzo.Tree.BinarySearchTree;public class sBinarySearchTree {
+package com.lugonzo.Tree.BinarySearchTree;
+
+public class sBinarySearchTree {
+
+   static class Node {
+        Node right,left;
+        int data;
+
+        Node (int d){
+            this.data = d;
+            left = right = null;
+        }
+    }
+
+    Node root;
+
+    sBinarySearchTree(int val){
+        root = new Node(val);
+    }
+
+    sBinarySearchTree(){
+        root = null;
+    }
 }
