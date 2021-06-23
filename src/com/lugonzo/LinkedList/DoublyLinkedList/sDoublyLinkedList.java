40e7abc3bb09909a6b38f96a923fb6260b838d6c
Abuu001 <abulugz@gmail.com>
2021-06-23 22:57:08 +0300

    impl2

--- src/com/lugonzo/LinkedList/DoublyLinkedList/sDoublyLinkedList.java
+++ src/com/lugonzo/LinkedList/DoublyLinkedList/sDoublyLinkedList.java
@@ -1,2 +1,75 @@
-package com.lugonzo.LinkedList.DoublyLinkedList;public class sDoublyLinkedList {
+package com.lugonzo.LinkedList.DoublyLinkedList;
+
+public class sDoublyLinkedList {
+
+    class Node {
+        Node prev;
+        Node next;
+        int data;
+
+        Node(int a){
+            this.data =a;
+        }
+    }
+
+    Node head;
+
+    public void addAtFirst(int data){
+
+        Node newNode = new Node(data);
+
+        newNode.next = head;
+        newNode.prev =null;
+
+        if(head != null){
+            head.prev =newNode;
+        }
+
+        head = newNode;
+    }
+
+    public void addNodeAtEnd(int data){
+
+        Node newNode = new Node(data);
+        Node last = head;
+
+        newNode.next =null;
+
+        // If the Linked List is empty, then make the new  node as head
+        if(head == null){
+            newNode.prev = null;
+            head =newNode;
+        }
+
+        while (last.next != null){
+            last = last.next;
+            last.next = newNode;
+
+            newNode.prev = last;
+        }
+
+    }
+
+    public void printAll(){
+
+        Node last = null;
+        Node curr = head;
+
+        while (curr != null){
+            System.out.print(curr.data + " => " );
+            curr = curr.next;
+        }
+
+    }
+
+    public void deleteNode(int value){
+
+        Node rem = head;
+        //if is head
+        if (rem.data == value){
+            head = rem.next;
+        }
+
+        // TODO incomplete
+    }
 }
