8051e0701aaef1d1a5d3f955663264695faa856a
Abuu001 <abulugz@gmail.com>
2021-07-19 22:04:03 +0300

    hashtable implementation

--- src/com/lugonzo/HashTable/HashTable.java
+++ src/com/lugonzo/HashTable/HashTable.java
@@ -1,6 +1,35 @@
 package com.lugonzo.HashTable;
 
+import java.util.Arrays;
+
 public class HashTable {
 
+  String[] theArray;
+  int arraySize;
+  int itemsInArray = 0;
 
+  public HashTable(int size){
+     arraySize = size;
+     theArray = new String[size];
+     Arrays.fill(theArray,"-1");
+  }
+
+  public void hashFunction(String[]stringForArray ,String[] theArray){
+     for(int n =0;n < stringForArray.length ; n++){
+      String newElementVal = stringForArray[n];
+      theArray[Integer.parseInt(newElementVal)] = newElementVal;
+     }
+  }
+
+
+ public static void main(String[] args) {
+     HashTable hash1 = new HashTable(30);
+     String[] elementsToAdd = {"1" , "5","17","21","26"};
+
+     hash1.hashFunction(elementsToAdd, hash1.theArray);
+     System.out.println(hash1);
+
+ }
 }
+
+
---
+++ src/com/lugonzo/HashTable/HashTableArray.java
@@ -0,0 +1,2 @@
+package com.lugonzo.HashTable;public class HashTableArray {
+}
